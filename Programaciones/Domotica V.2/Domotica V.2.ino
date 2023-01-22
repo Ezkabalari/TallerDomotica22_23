de764a084991f29ee70b95194d446483774d87a3
04aromeo <89753924+04aromeo@users.noreply.github.com>
2023-01-22 20:35:49 +0100

    Update Domotica V.2.ino

--- Programaciones/Domotica V.2/Domotica V.2.ino
+++ Programaciones/Domotica V.2/Domotica V.2.ino
@@ -1,14 +1,14 @@
 /*
  * Pines de conexionado a arduino UNO:
  * 
- * A0: Sensor de gas para la cocina
- * A1: Sensor de humedad de la tierra
- * A2: Sensor de luz para garaje
- * A3: Sensor de luz para exterior
- * A4: Sensor de presencia del pasillo
- * A5: Sensor de temperatura de habitación 1
- * D0: Nextion cable azul
- * D1: Nextion cable amarillo
+ * A0: Sensor de gas para la cocina           (MQ-02)
+ * A1: Sensor de humedad de la tierra         (Higrometro V1.2)
+ * A2: Sensor de luz para garaje              (KY-018)
+ * A3: Sensor de luz para exterior            (KY-018)
+ * A4: Sensor de presencia del pasillo        (HC-SR505)
+ * A5: Sensor de temperatura de habitación 1  (LM35)
+ * D0: Nextion cable azul                     (Nextion NX8048T050)
+ * D1: Nextion cable amarillo                 (Nextion NX8048T050)
  * D2: Leds Habitación 1
  * D3: Leds Baño
  * D4: Leds Cuarto lavadora
@@ -16,20 +16,20 @@
  * D6: Leds pasillo
  * D7: Leds Cocina
  * D8: Leds Salón
- * D9: Leds Gimnasio
- * D10: Servomotor del garaje (Valla)
- * D11: Servomotor de la puerta principal
- * D12: Ventilador de la habitación 1
- * D13: Bomba de agua de la tierra
+ * D9: Leds Gimnasio    
+ * D10: Servomotor del garaje (Valla)         (SG-90)
+ * D11: Servomotor de la puerta principal     (SG-90)
+ * D12: Ventilador de la habitación 1         (Noctua NF-A4x10)
+ * D13: Bomba de agua de la tierra            (Bomba de agua de 12v)
  */
 
 
 void setup() {
-  // put your setup code here, to run once:
+  
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 
 }
