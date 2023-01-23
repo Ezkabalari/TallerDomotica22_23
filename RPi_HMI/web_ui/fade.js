9fba1b64f71a0ac62551ce6b214f854d2c8de40c
Ezkabalari <ojoristain@gmail.com>
2023-01-23 13:20:06 +0100

    Añadidos retoques a web UI usando ejemplo. Sin usar johny-five.

--- RPi_HMI/web_ui/fade.js
+++ RPi_HMI/web_ui/fade.js
@@ -1,12 +1,18 @@
-const express = require(‘express’);
+const express = require('express');
 const app = express();
-const http = require(‘http’);
+const http = require('http');
 const server = http.createServer(app);
-app.get(‘/’, (req, res) => {
-     res.sendFile(__dirname + ‘/public/index.html’);
+// create a serial port ref https://stackoverflow.com/questions/52541287/node-serialport-is-not-a-constructor
+var SerialPort = require('serialport');
+var port = new SerialPort('/dev/ttyACM0',{ baudRate: 9600});
+var dt = require('./examples/gettimedate');
+
+app.get('/', (req, res) => {
+     res.sendFile(__dirname + '/public/index.html');
+     //console.log(dt.myDateTime()+' : Movement on slider requested.');
 })
 
-server.listen(8080, () => {
-     console.log(‘listening on *:8080’);
+server.listen(8000, () => {
+     console.log('listening on *:8080');
 });
-console.log(“Web Server Started go to ‘http://10.27.8.9:8080’ in your Browser.”);
+console.log("Servidor web arrancado. Visita la URL 'http://10.27.8.9:8000' in your Browser.");
